d255bbe0248acb172e4c0129cb8afe0709a42d16
nniikon <kolya.kasparov@gmail.com>
2025-12-07 21:01:50 +0300

    add float registers

--- include/rvi_registers.hpp
+++ include/rvi_registers.hpp
@@ -32,4 +32,24 @@ private:
     std::array<uint32_t, kNumRegs> regs_{};
 };
 
+class InterpreterRegistersFloat {
+public:
+    InterpreterRegistersFloat() = default;
+
+    float Get(uint32_t index) const noexcept {
+        assert(index < static_cast<uint32_t>(kNumRegs));
+
+        return regs_[index];
+    }
+
+    void Set(uint32_t index, float value) {
+        assert(index < static_cast<uint32_t>(kNumRegs));
+
+        regs_[index] = value;
+    }
+
+private:
+    std::array<float, kNumRegs> regs_{};
+};
+
 } // namespace rvi
