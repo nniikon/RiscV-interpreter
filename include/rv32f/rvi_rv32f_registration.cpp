e6be26a3ab600afbb98d04511b6ec56105f46c81
nniikon <kolya.kasparov@gmail.com>
2025-12-07 21:43:13 +0300

    implement fsw

--- include/rv32f/rvi_rv32f_registration.cpp
+++ include/rv32f/rvi_rv32f_registration.cpp
@@ -1,8 +1,10 @@
 #include "rvi_rv32f_registration.hpp"
 #include "rv32f/rvi_rv32f_type_f_load.hpp"
+#include "rv32f/rvi_rv32f_type_s_save.hpp"
 
 using namespace rvi;
 
 void rvi::rv32f::RegisterRV32F(InstructionRegistry* registry) {
     rvi::rv32f::RegisterOpcodeGroupTypeI_Flw(registry);
+    rvi::rv32f::RegisterOpcodeGroupTypeS_Fsw(registry);
 }
